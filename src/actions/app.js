5256a966f3356017bb7a9febddfa48bb38230be8
Wesley Luyten <hi@wesleyluyten.com>
2019-02-09 14:52:27 -0500

    Make sure page is loaded before dispatch

--- src/actions/app.js
+++ src/actions/app.js
@@ -23,25 +23,26 @@ export const navigate = path => dispatch => {
 };
 
 const loadPage = page => dispatch => {
+  let pagePromise;
   switch (page) {
     case 'view1':
-      import('../components/my-view1.js').then(module => {
+      pagePromise = import('../components/my-view1.js').then(module => {
         // Put code in here that you want to run every time when
         // navigating to view1 after my-view1.js is loaded.
       });
       break;
     case 'view2':
-      import('../components/my-view2.js');
+      pagePromise = import('../components/my-view2.js');
       break;
     case 'view3':
-      import('../components/my-view3.js');
+      pagePromise = import('../components/my-view3.js');
       break;
     default:
       page = 'view404';
-      import('../components/my-view404.js');
+      pagePromise = import('../components/my-view404.js');
   }
 
-  dispatch(updatePage(page));
+  pagePromise.then(() => dispatch(updatePage(page)));
 };
 
 const updatePage = page => {
